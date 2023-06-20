a6f6e7cb64c70107d8e193274787e0a508157f84
KeithDinh <keithdinhcs@gmail.com>
2023-06-20 07:47:55 -0500

    add AOC day 5 2015

---
+++ AOC/2015/AOC 2015/Day4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+string input = "iwrupvqb";
+
+int count = 30000; // Threshold: 9958218
+
+while (true)
+{
+    string md5Hash = ComputeMD5Hash(input + count.ToString());
+    if (md5Hash.Substring(0, 6) == "000000")
+    {
+        break;
+    }
+    count++;
+}
+
+Console.WriteLine($"Part 2: {count}");
+string ComputeMD5Hash(string input)
+{
+    using (MD5 md5 = MD5.Create())
+    {
+        byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+        byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < hashBytes.Length; i++)
+        {
+            builder.Append(hashBytes[i].ToString("x2")); // Convert each byte to a 2-digit hexadecimal representation
+        }
+
+        return builder.ToString();
+    }
+}
---
+++ AOC/2015/AOC 2015/Day5/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+/**
+ * TODO: fetch input from URL using token or pull AOC session from browser cookies
+ */
+List<char> vowels = new List<char>() { 'a', 'e', 'o', 'u', 'i' };
+List<string> excludes = new List<string>() { "ab", "cd", "pq", "xy" };
+try
+{
+    int niceStringCountPart1 = 0, niceStringCountPart2 = 0;
+    // Open the text file using a StreamReader
+    using (StreamReader sr = new StreamReader(getFilePath()))
+    {
+        string line;
+
+        // Read and display lines from the file until the end is reached
+        while ((line = sr.ReadLine()) != null)
+        {
+            if (isNiceStringPart1(line.Trim()))
+                niceStringCountPart1++;
+            if (isNiceStringPart2(line.Trim()))
+                niceStringCountPart2++;
+        }
+    }
+
+    // Part 1
+    Console.WriteLine(niceStringCountPart1);
+    // Part 2
+    Console.WriteLine(niceStringCountPart2);
+}
+catch (Exception e)
+{
+    // Handle any exceptions that occur during file reading
+    Console.WriteLine("An error occurred while reading the file: " + e.Message);
+}
+string getFilePath()
+{
+    // This will get the current WORKING directory (i.e. \bin\Debug)
+    string workingDirectory = Environment.CurrentDirectory;
+    // or: Directory.GetCurrentDirectory() gives the same result
+
+    // This will get the current PROJECT bin directory (ie ../bin/)
+    string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+
+    // This will get the current PROJECT directory
+    string projectDirectory2 = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+
+    string filePath = $"{projectDirectory2}\\input"; // Specify the file name or path
+
+    return filePath;
+}
+bool isNiceStringPart1(string s)
+{
+    if (s.Length == 0) return false;
+
+    int countVowel = 0;
+    bool containExcludes = false,
+        hasDoubleChar = false;
+
+    for(int i = 0; i < s.Length; i++)
+    {
+        if (vowels.Contains(s[i])) 
+            countVowel++; 
+
+        if(i+1 < s.Length && excludes.Contains(s.Substring(i, 2)))
+            containExcludes = true;
+
+        if (i + 1 < s.Length && s[i] == s[i+1])
+            hasDoubleChar = true;
+    }
+    if (countVowel < 3 || containExcludes || !hasDoubleChar) { return false;  }
+
+    return true;
+}
+bool isNiceStringPart2(string s)
+{
+    if (s.Length == 0) return false;
+
+    bool hasRightPair = false,
+        hasLetterRepeat = false;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        try
+        {
+            if (i + 2 < s.Length && s[i] == s[i + 2])
+                hasLetterRepeat = true;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine('1');
+            throw;
+        }
+
+        try
+        {
+            string rest = (i < s.Length - 3) ? s.Substring(i + 2) : "";
+
+            if (i+1 < s.Length && 
+                rest.IndexOf(s[i].ToString() + s[i + 1].ToString()) > -1)
+            {
+                hasRightPair = true;
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine('2');
+            throw;
+        }
+
+    }
+    if(!hasLetterRepeat || !hasRightPair) { return false; }
+    return true;
+}
